f931765a4b9589928874898a1809189eec2bf435
CawanTavares <tavarescawan6@gmail.com>
2022-04-15 00:46:21 -0300

    LISTA 1 - LP1 - T03

--- minmax/src/function.cpp
+++ minmax/src/function.cpp
@@ -1,19 +1,31 @@
 #include "function.h"
 
-/*! 
- * Finds and returns a pair with the first instance of the smallest element
- * and the last instance of the largest element in an array.
- *
- * @param V This is the array-to-pointer decay representing an array.
- * @param n The array's size.
- *
- * @return A pair of indexes to the first smallest and last largest values.
- */
 
 std::pair<int,int> min_max( int V[], size_t n )
 {
-    // TODO: Adicione aqui sua solução.
-
-    // TODO: Isso é apenas um STUB. Substitua com seu retorno correto.
-    return { -1, -1 };
+	int Vmax = A[0];
+	int Vmin = A[0];
+	std::size_t VmaxPos = 0; 
+	std::size_t VminPos = 0; 
+	
+	if (n == 0) std::make_pair(-1,-1); // VAZIO
+		
+	for (std::size_t i = 0; i < n; i++)
+	{
+		if (A[i] < Vmin)
+		{
+		   Vmin = A[i];
+		   Vmin = i;	  	   	
+			
+		}	
+		if (A[i] > Vmax)
+		{
+		    Vmax = A[i];
+			Vmax = i;	   	
+		}
+	}
+	
+		
+	
+    return std::make_pair(Vmin, Vmax);;
 }
--- minmax/src/function.h
+++ minmax/src/function.h
@@ -2,6 +2,7 @@
 #define FUNCTION_H
 
 #include <utility>
+#include <cstdlib>
 using std::pair;
 
 std::pair<int, int> min_max( int V[], std::size_t n );
